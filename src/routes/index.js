fa43e7aad2809b5d88218f046723484876fbd85c
ibrahim013 <walaibrahim13@gmail.com>
2019-02-03 08:17:37 +0100

    delete contact and message route functionality

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,22 @@
 import express from 'express';
 import Contact from '../controller/index';
 
-const { addContact, sendMessage, getSentMessages } = Contact;
+const {
+  addContact,
+  sendMessage,
+  getSentMessages,
+  getContact,
+  deleteContact,
+  deleteMessage,
+} = Contact;
 
 const router = express.Router();
 
 router.post('/contact', addContact);
+router.get('/contact', getContact);
+router.delete('/contact/:phoneNumber', deleteContact);
 router.post('/message', sendMessage);
 router.get('/message', getSentMessages);
+router.delete('/message/:messageId/contacts/:contactId', deleteMessage);
 
 export default router;
