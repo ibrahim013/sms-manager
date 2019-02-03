aa22b0aca5dee6d9e6cc8307e4a12036e1dd9682
ibrahim013 <walaibrahim13@gmail.com>
2019-02-03 08:17:37 +0100

    delete contact and message route functionality

--- src/controller/index.js
+++ src/controller/index.js
@@ -2,13 +2,12 @@ import validateContact from '../validator/contactValidator';
 import validateMessage from '../validator/messagingValidator';
 import Contacts from '../models/Contact';
 import Messaging from '../models/Messaging';
-/**
- * @description this handle adding of contact
- */
+
 
 class Contact {
 /**
- * @description this is a method that add a new contact to the database
+ * Route: POST: /api/v1/contact
+ * @description This method adds user contact
  * @param {object} req
  * @param {object} res
  */
@@ -31,9 +30,11 @@ class Contact {
   }
 
   /**
-   * @description this is a method that add a new contact to the database
+   * Routes: POST: /api/v1/message
+   * @description This method sends message
    * @param {object} req
    * @param {object} res
+   * @returns {object}
    */
   static sendMessage(req, res) {
     const { errors, isValid } = validateMessage(req.body);
@@ -59,7 +60,7 @@ class Contact {
                 },
                 receiver: {
                   id: receiverContact._id,
-                  phoneNumber: req.body.senderNumber,
+                  phoneNumber: req.body.receiverNumber,
                   name: receiverContact.name,
                 },
                 message: req.body.message,
@@ -74,7 +75,11 @@ class Contact {
   }
 
   /**
-   * @description GET: /api/v1/message?sender=messageId or ?receiver=messageId
+   * Routes: GET: /api/v1/message?sender=messageId or ?receiver=messageId
+   * @description  This method gets messages by sender or receiver
+   * @param {object} req
+   * @param {object} res
+   * @returns {object}
    */
   static getSentMessages(req, res) {
     const requestSender = req.query.sender;
@@ -84,18 +89,17 @@ class Contact {
     }
     if (requestSender) {
       Messaging.findOne({ _id: requestSender.trim() }).then((msg) => {
-        res.status(400).json({
+        res.status(200).json({
           sender: msg.sender.name,
           message: msg.message,
           date: msg.createdAt,
           status: 'success',
-        })
-          .catch(() => (res.status(404).json({ msg: 'something went wrong', status: 'fail' })));
-      });
+        });
+      }).catch(() => (res.status(404).json({ msg: 'something went wrong', status: 'fail' })));
     }
     if (requestReceiver) {
       Messaging.findOne({ _id: requestReceiver.trim() }).then((msg) => {
-        res.status(400).json({
+        res.status(200).json({
           receiver: msg.receiver.name,
           message: msg.message,
           date: msg.createdAt,
@@ -103,6 +107,89 @@ class Contact {
         });
       }).catch(() => (res.status(404).json({ msg: 'message not found', status: 'fail' })));
     }
+  }
+
+  /**
+   * Routes: GET: /api/v1/contacts?contact=phoneNumber
+   * @description This method gets a users contact
+   * @param {object} req
+   * @param {object} res
+   * @returns {object}
+   */
+  static getContact(req, res) {
+    // eslint-disable-next-line prefer-destructuring
+    const phoneNumber = req.query.contact.trim();
+    if (!phoneNumber) {
+      res.status(400).json({ msg: 'unknown request' });
+    }
+    if (phoneNumber) {
+      Contacts.findOne({ phoneNumber }).then((user) => {
+        res.status(200).json({
+          id: user._id,
+          name: user.name,
+          phoneNumber: user.phoneNumber,
+          date: user.createdAt,
+          status: 'success',
+        });
+      }).catch(() => (res.status(404).json({ msg: 'contact not found', status: 'fail' })));
+    }
+  }
+
+  /**
+   * Routes: DELETE: /api/v1/contacts/:phoneNumber
+   * @description This deletes a user contact
+   * @param {object} req
+   * @param {object} res
+   * @returns {object}
+   */
+  static deleteContact(req, res) {
+    // eslint-disable-next-line prefer-destructuring
+    const contact = req.params.phoneNumber.trim();
+    if (!contact) {
+      res.status(400).json({ msg: 'unknown request' });
+    }
+    if (contact) {
+      Contacts.findOne({ phoneNumber: contact }).then((contact) => {
+        Contacts.remove({ _id: contact._id }).then(() => {
+          res.status(202).json({
+            msg: 'contact removed sucessfully',
+            status: 'success',
+          });
+        }).catch(() => (res.status(404).json({ msg: 'something went wrong', status: 'fail' })));
+      });
+    }
+  }
+
+  /**
+   * Routes: DELETE: /api/v1/message/:messageId/contacts/:contactId
+   * @description This deletes message created by a user
+   * @param {object} req
+   * @param {oblect} res
+   * @return {void}
+   */
+  static deleteMessage(req, res) {
+    const messageId = req.params.messageId.trim();
+    const contactId = req.params.contactId.trim();
+    if (!(req.params.messageId || req.params.contactId)) {
+      return res.status(401).json({
+        status: false,
+        msg: ' Unauthorized to perform this operation',
+      });
+    }
+    Messaging.find({}).where({ _id: messageId, 'sender.id': contactId })
+      .then((message) => {
+        if (!message) {
+          return res.status(404).json({
+            msg: 'message does not exist',
+            status: 'fail',
+          });
+        }
+        Mesaging.remove({ _id: messageId })
+          .then(() => res.status(202).send({
+            status: true,
+            msg: 'Message deleted successfully',
+          }));
+      }).catch(() => (res.status(500).json({ msg: 'something went wrong', status: 'fail' })));
   }
 }
 export default Contact;
